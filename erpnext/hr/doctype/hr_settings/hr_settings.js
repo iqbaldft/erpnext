63cda712e8a83050b07c7f99bb4b00ed857c49fb
Iqbal Dhea F T <iqbal.dft@gmail.com>
2019-05-14 15:21:43 +0700

    settings default attendance status

--- erpnext/hr/doctype/hr_settings/hr_settings.js
+++ erpnext/hr/doctype/hr_settings/hr_settings.js
@@ -4,5 +4,35 @@
 frappe.ui.form.on('HR Settings', {
 	refresh: function(frm) {
 
+	},
+	onload: function(frm) {
+		frm.set_query("default_present_status", function(){
+			return {
+				"filters": {
+					"attendance_status": "Present"
+				}
+			};
+		});
+		frm.set_query("default_half_day_status", function(){
+			return {
+				"filters": {
+					"attendance_status": "Half Day"
+				}
+			};
+		});
+		frm.set_query("default_on_leave_status", function(){
+			return {
+				"filters": {
+					"attendance_status": "On Leave"
+				}
+			};
+		});
+		frm.set_query("default_absent_status", function(){
+			return {
+				"filters": {
+					"attendance_status": "Absent"
+				}
+			};
+		});
 	}
 });
--- erpnext/hr/doctype/hr_settings/hr_settings.py
+++ erpnext/hr/doctype/hr_settings/hr_settings.py
@@ -5,6 +5,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 
 from frappe.model.document import Document
 
@@ -13,3 +14,16 @@ class HRSettings(Document):
 		from erpnext.setup.doctype.naming_series.naming_series import set_by_naming_series
 		set_by_naming_series("Employee", "employee_number",
 			self.get("emp_created_by")=="Naming Series", hide_name_field=True)
+		self.validate_default_attendance_status()
+
+	def validate_default_attendance_status(self):
+		def validate_status(field_status, expected):
+			if not field_status:
+				return
+			status = frappe.get_cached_value("Attendance Status", field_status, "attendance_status")
+			if status != expected:
+				frappe.throw("{} {}".format(_("Invalid attendance status:"), field_status))
+		validate_status(self.default_present_status, "Present")
+		validate_status(self.default_half_day_status, "Half Day")
+		validate_status(self.default_on_leave_status, "On Leave")
+		validate_status(self.default_absent_status, "Absent")
