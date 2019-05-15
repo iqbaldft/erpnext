df3b383f3b2994e15f6ff85705e99d3dbdaf0c36
Iqbal Dhea F T <iqbal.dft@gmail.com>
2019-05-15 14:43:40 +0700

    init get attendance rule based on employee, from date

--- erpnext/hr/doctype/attendance_rule_assignment/attendance_rule_assignment.py
+++ erpnext/hr/doctype/attendance_rule_assignment/attendance_rule_assignment.py
@@ -5,6 +5,25 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class AttendanceRuleAssignment(Document):
 	pass
+
+
+def get_attendance_rule(employee, from_date, company=None, department=None, not_found_error=False):
+	filters={
+		"employee": employee,
+		"from_date": ["<=", from_date],
+		"company": company or frappe.get_value("Employee", employee, "company")
+	}
+	if department:
+		filters["department"] = department
+	attendance_rule = frappe.get_all("Attendance Rule", filters=filters, order_by="from_date desc")
+	if len(attendance_rule) > 0:
+		return attendance_rule[0].attendance_rule
+	elif not_found_error:
+		frappe.throw(_("Attendance rule not found for employee {0} from date {1}").format(employee, from_date))
+	else:
+		return None
+
