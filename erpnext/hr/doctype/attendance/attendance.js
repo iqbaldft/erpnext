7d450eb17d611c9301e746e83406e3755d79bbf4
Iqbal Dhea F T <iqbal.dft@gmail.com>
2019-05-20 12:12:39 +0700

    get attendance rule clear status if check in / out time is changed

--- erpnext/hr/doctype/attendance/attendance.js
+++ erpnext/hr/doctype/attendance/attendance.js
@@ -13,3 +13,34 @@ cur_frm.fields_dict.employee.get_query = function(doc,cdt,cdn) {
 		query: "erpnext.controllers.queries.employee_query"
 	}	
 }
+
+frappe.ui.form.on('Attendance', {
+	refresh: function(frm) {
+
+	},
+	employee: function(frm){
+		if(frm.doc.employee && frm.doc.attendance_date){
+			frappe.call({
+				method: "erpnext.hr.doctype.attendance.attendance.get_attendance_rule",
+				args: {
+					"employee": frm.doc.employee,
+					"attendance_date": frm.doc.attendance_date
+				},
+				callback: function(r){
+					if(r.message){
+						frm.set_value("attendance_rule", r.message)
+					}
+				}
+			});
+		}
+	},
+	attendance_date: function(frm){
+		frm.trigger("employee");
+	},
+	check_in_time: function(frm){
+		frm.set_value("status", "");
+	},
+	check_out_time: function(frm){
+		frm.set_value("status", "");
+	}
+});
