0a61f2c25a1628116bedf36cb4d8047111f1cba7
Iqbal Dhea F T <iqbal.dft@gmail.com>
2019-05-20 12:54:00 +0700

    add batch assignment

--- erpnext/hr/doctype/attendance_rule/attendance_rule.js
+++ erpnext/hr/doctype/attendance_rule/attendance_rule.js
@@ -3,6 +3,60 @@
 
 frappe.ui.form.on('Attendance Rule', {
 	refresh: function(frm) {
-
+		if(frm.doc.docstatus == 1){
+			frm.add_custom_button(__("Assign Attendance Rule"), function(){
+				let doc = frappe.model.get_new_doc("Attendance Rule Assignment");
+				doc.attendance_rule = frm.doc.name;
+				doc.company = frm.doc.company;
+				frappe.set_route("Form", "Attendance Rule Assignment", doc.name);
+			});
+			frm.add_custom_button(__("Assign to Employees"), function(){
+				frm.trigger("assign_to_employee");
+			})
+		}
+	},
+	assign_to_employee: function(frm){
+		var d = new frappe.ui.Dialog({
+			title: __("Assign to Employees"),
+			fields: [
+				{
+					fieldname: "sec_break",
+					fieldtype: "Section Break",
+					label: __("Filter Employees By (Optional)")
+				}, {
+					fieldname: 'department',
+					fieldtype: 'Link',
+					options: 'Department',
+					label: __('Department')
+				}, {
+					fieldname: "employee",
+					fieldtype: "Link",
+					options: "Employee",
+					label: __("Employee")
+				},
+				{
+					fieldname: 'from_date',
+					fieldtype: 'Date',
+					label: __('From Date'),
+					"reqd": 1
+				},
+			],
+			primary_action: function(){
+				var data = d.get_values();
+				frappe.call({
+					doc: frm.doc,
+					method: "assign_attendance_rule",
+					args: data,
+					callback: function(r){
+						if(!r.exc){
+							d.hide();
+							frm.reload_doc();
+						}
+					}
+				});
+			},
+			primary_action_label: __("Assign")
+		});
+		d.show();
 	}
 });
--- erpnext/hr/doctype/attendance_rule/attendance_rule.py
+++ erpnext/hr/doctype/attendance_rule/attendance_rule.py
@@ -5,6 +5,67 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class AttendanceRule(Document):
-	pass
+	@frappe.whitelist()
+	def assign_attendance_rule(self, department=None, employee=None, from_date=None):
+		employees = self.get_employees(department=department, name=employee, company=self.company)
+		# assign attendance rule
+		if employees:
+			if len(employees) > 20:
+				frappe.enqueue(
+					assign_attendance_rule_for_employee,
+					timeout=600,
+					employees=employees,
+					attendance_rule=self,
+					from_date=from_date,
+				)
+			else:
+				assign_attendance_rule_for_employee(
+					employees, self, from_date=from_date
+				)
+		else:
+			frappe.msgprint(_("No Employee Found"))
+
+	def get_employees(self, **kwargs):
+		conditions, values = [], []
+		for field, value in kwargs.items():
+			if value:
+				conditions.append("{0}=%s".format(field))
+				values.append(value)
+		condition_str = " and " + " and ".join(conditions) if conditions else ""
+
+		employees = frappe.db.sql_list(
+			"select name from tabEmployee "
+			"where status='Active'{condition}".format(condition=condition_str),
+			tuple(values),
+		)
+		return employees
+
+
+def assign_attendance_rule_for_employee(employees, attendance_rule, from_date=None):
+    attendance_rule_assignments = []
+    count = 0
+    for employee in employees:
+        count += 1
+
+        assignment = create_attendance_rule_assignment(
+            employee, attendance_rule, from_date
+        )
+        attendance_rule_assignments.append(assignment)
+        frappe.publish_progress(
+            count * 100 / len(employees), title=_("Assigning Attendance Rule")
+        )
+    if attendance_rule_assignments:
+        frappe.msgprint(_("Structures have been assigned successfully"))
+
+
+def create_attendance_rule_assignment(employee, attendance_rule, from_date):
+    assignment = frappe.new_doc("Attendance Rule Assignment")
+    assignment.employee = employee
+    assignment.attendance_rule = attendance_rule.name
+    assignment.from_date = from_date
+    assignment.save(ignore_permissions=True)
+    assignment.submit()
+    return assignment.name
